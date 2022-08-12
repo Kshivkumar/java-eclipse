93bf655b07f161f4f190750d9d5b0081f6141888
Ccare <shivkumar.xworkz@gmail.com>
2022-08-13 00:01:22 +0530

    Integer

---
+++ src/com/xworkz/program/IntegersPirates.java
@@ -0,0 +1,44 @@
+package com.xworkz.program;
+
+public class IntegersPirates {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Integer inter = new Integer(23);
+		byte gh = inter.byteValue();
+
+		System.out.println(gh);
+		inter = inter.compareTo(23);
+		System.out.println(inter);
+		double gfdd = inter.doubleValue();
+		System.out.println(gfdd);
+
+		int cash = inter.intValue();
+		System.out.println(cash);
+		
+		long git=inter.longValue();
+		System.out.println(git);
+		
+		float mobile=inter.floatValue();  
+		System.out.println(mobile);
+		
+		 int watch=inter.hashCode();
+		 System.out.println(watch);
+		 
+		 //decode(String nm)
+	   int a=inter.decode("123");
+		System.out.println(a);
+		
+	     String	name="34";
+		Integer am=Integer.valueOf(name);
+		System.out.println(am);
+		
+		//highestOneBit(int i)_
+		int name1=36;
+		//Integer am1=Integer.SIZE(name1);
+		System.out.println(name1);
+
+	}
+
+}
