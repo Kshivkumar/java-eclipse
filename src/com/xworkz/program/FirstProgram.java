b11e309f2e389cfdbb389a30f53d3505700d9605
Ccare <shivkumar.xworkz@gmail.com>
2022-08-12 12:38:20 +0530

    newprogram

---
+++ src/com/xworkz/program/FirstProgram.java
@@ -0,0 +1,10 @@
+package com.xworkz.program;
+
+public class FirstProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Shivu");
+	}
+
+}
